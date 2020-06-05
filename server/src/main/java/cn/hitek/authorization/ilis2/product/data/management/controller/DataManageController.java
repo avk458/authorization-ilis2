ba50a590e6dcd0a15c224771bac7ea4e211e8636
kano <avk458@gmail.com>
2020-06-05 13:44:38 +0800

    remove mybaits puls dynamic datasource

--- server/src/main/java/cn/hitek/authorization/ilis2/product/data/management/controller/DataManageController.java
+++ server/src/main/java/cn/hitek/authorization/ilis2/product/data/management/controller/DataManageController.java
@@ -7,9 +7,6 @@ import cn.hitek.authorization.ilis2.product.data.management.domain.DatabaseInfo;
 import cn.hitek.authorization.ilis2.product.data.management.service.DataManageService;
 import cn.hitek.authorization.ilis2.product.database.domain.UnitDatabase;
 import cn.hitek.authorization.ilis2.product.database.service.UnitDatabaseService;
-import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
-import com.baomidou.dynamic.datasource.creator.HikariDataSourceCreator;
-import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DataSourceProperty;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,10 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.sql.DataSource;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
-import java.util.Set;
 
 
 /**
@@ -32,8 +27,6 @@ import java.util.Set;
 @AllArgsConstructor
 public class DataManageController {
 
-    private final DataSource dataSource;
-    private final HikariDataSourceCreator hikariDataSourceCreator;
     private final DataManageService dataManageService;
     private final UnitDatabaseService unitDatabaseService;
     private final ConfigService configService;
@@ -58,26 +51,5 @@ public class DataManageController {
     public Response getConfigStandardDatabase(@NotBlank(message = "配置信息参数有误") String configId) {
         String schema = this.dataManageService.getConfigStandardDatabase(configId);
         return new Response().code(HttpStatus.OK).data(schema);
-    }
-
-
-    @GetMapping
-    public Set<String> now() {
-        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
-        return ds.getCurrentDataSources().keySet();
-    }
-
-    @PostMapping("/addHikariCP")
-    public Set<String> addHikariCP() {
-        DataSourceProperty dataSourceProperty = new DataSourceProperty();
-        dataSourceProperty.setPoolName("test");
-        dataSourceProperty.setDriverClassName("com.p6spy.engine.spy.P6SpyDriver");
-        dataSourceProperty.setUrl("jdbc:p6spy:mysql://127.0.0.1:3306/ilis_local?characterEncoding=utf8&serverTimezone=Asia/Shanghai&useSSL=false");
-        dataSourceProperty.setUsername("root");
-        dataSourceProperty.setPassword("123456");
-        DynamicRoutingDataSource ds = (DynamicRoutingDataSource) dataSource;
-        DataSource dataSource = hikariDataSourceCreator.createDataSource(dataSourceProperty);
-        ds.addDataSource(dataSourceProperty.getPoolName(), dataSource);
-        return ds.getCurrentDataSources().keySet();
     }
 }
